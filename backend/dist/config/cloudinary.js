ca369c97a94fc1dedfe2d7cd2d3bf0c4f241278f
Bablu Verma <jbabluverma@gmail.com>
2025-11-16 21:01:04 +0530

    change some erro in server

---
+++ backend/dist/config/cloudinary.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cloudinary_config = void 0;
+const cloudinary_1 = require("cloudinary");
+const cloudinary_config = async () => {
+    // Configuration
+    cloudinary_1.v2.config({
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    return cloudinary_1.v2;
+};
+exports.cloudinary_config = cloudinary_config;
